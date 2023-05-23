a7ddc6a235b61a7f34a5b8742b9dfe9bedd15676
Erich Engler <erich.engler@hotmail.com>
2023-05-23 04:41:41 -0500

    made changes to Nav component. removed logout button and assigned logout dispatch to text below navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,45 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogOutButton from './LogOutButton/LogOutButton';
 import './Nav.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Nav() {
+
+	const dispatch = useDispatch();
+
+	// ------- Storing user information -------
 	const user = useSelector((store) => store.user);
 
+	// ------- Logs out user -------
+	const handleLogout = () => {
+		dispatch({ type: 'LOGOUT' });
+	}
+
 	return (
-		<div className="nav">
-			<Link to="/home">
-				<h2 className="nav-title">Pix</h2>
-			</Link>
-			<div>
-				{/* If no user is logged in, show these links */}
-				{!user.id && (
-					// If there's no user, show login/registration links
-					<Link className="navLink" to="/login">
-						Login
-					</Link>
-				)}
-
-				{/* If a user is logged in, show these links */}
-				{user.id && (
-					<>
-						<Link className="navLink" to="/champions">
-							Champions
-						</Link>
-
-						<Link className="navLink" to="/favorites">
-							Favorites
-						</Link>
-
-						<LogOutButton className="navLink" />
-					</>
-				)}
-
-				<Link className="navLink" to="/about">
-					About
+		<div>
+			<div className="nav">
+				<Link to="/home">
+					<h2 className="nav-title">Pix</h2>
 				</Link>
+				<div>
+					{/* -------If no user is logged in, show these ------- */}
+					{!user.id && (
+						<>
+							{/* ------- Login link ------- */}
+							<Link className="navLink" to="/login">
+								Login
+							</Link>
+
+							{/* ------- Register link ------- */}
+							<Link className="navLink" to="/registration">
+								Register
+							</Link>
+						</>
+					)}
+
+					{/* ------- If a user is logged in, show these ------- */}
+					{user.id && (
+						<>
+							{/* ------- Champions link ------- */}
+							<Link className="navLink" to="/champions">
+								Champions
+							</Link>
+
+							{/* ------- Favorites link ------- */}
+							<Link className="navLink" to="/favorites">
+								Favorites
+							</Link>
+						</>
+					)}
+
+					{/* ------- About link ------- */}
+					<Link className="navLink" to="/about">
+						About
+					</Link>
+				</div>
 			</div>
+
+			{/* ------- If a user is logged in,  show these ------- */}
+			{user.id && (
+				<div className="signed-in-section">
+					<span className="signed-in-text">
+						Logged in as {user.username}
+					</span>
+					|
+					<span className="logout-text" onClick={handleLogout}>
+						Logout
+					</span>
+				</div>
+			)}
 		</div>
 	);
 }
